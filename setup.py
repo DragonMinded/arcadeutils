f57fbe21a850b0eafa736cde11c942d8d5b602d0
Jennifer Taylor <dragonminded@dragonminded.com>
2021-10-04 15:16:58 +0000

    Rename description to readme so github displays it by default.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import os
 from setuptools import setup  # type: ignore
 
 
-with open(os.path.join("arcadeutils", "DESCRIPTION.md"), "r", encoding="utf-8") as fh:
+with open(os.path.join("arcadeutils", "README.md"), "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
@@ -16,7 +16,7 @@ setup(
     author_email='dragonminded@dragonminded.com',
     license='Public Domain',
     url='https://github.com/DragonMinded/arcadeutils',
-    package_data={"arcadeutils": ["py.typed", "DESCRIPTION.md"]},
+    package_data={"arcadeutils": ["py.typed", "README.md"]},
     packages=[
         'arcadeutils',
     ],
